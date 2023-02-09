04ff60926721a1c0de2b757be8c3af83a369fbde
sravyakanamarlapudi <119756083+sravyakanamarlapudi@users.noreply.github.com>
2023-02-09 10:38:13 +0530

    Update receiveMail.java

--- receiveMail.java
+++ receiveMail.java
@@ -1,65 +1,56 @@
+import java.util.Scanner;
 import java.util.Properties;
-import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
-import com.sun.mail.pop3.POP3Store;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
-public class ReceiveMail{
 
-    public static void receiveEmail(String pop3Host, String storeType,String user, String password)
-    {
-        try {
 
-            //create properties field
-            Properties properties = new Properties();
+public class ReceiveMail {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
 
-            properties.put("mail.pop3.host", host);
-            properties.put("mail.pop3.port", "995");
-            properties.put("mail.pop3.starttls.enable", "true");
-            Session emailSession = Session.getDefaultInstance(properties);
+        Scanner scanner = new Scanner(System.in);
 
-            //create the POP3 store object and connect with the pop server
-            Store store = emailSession.getStore("pop3s");
+        System.out.println("username for authentication: ");
+        final String username = scanner.nextLine();
 
-            store.connect(host, user, password);
+        System.out.println("password for authentication: ");
+        final String password = scanner.nextLine();
 
-            //create the folder object and open it
-            Folder emailFolder = store.getFolder("INBOX");
-            emailFolder.open(Folder.READ_ONLY);
+        System.out.println("to email: ");
+        String toEmailAddress = scanner.nextLine();
 
-            // retrieve the messages from the folder in an array and print it
-            Message messages [] = emailFolder.getMessages();
-            int i = ((messages.length)-1);
+        System.out.println("subject: ");
+        String subject = scanner.nextLine();
 
-            Message message = messages(i);
+        System.out.println("message: ");
+        String textMessage = scanner.nextLine();
 
-            System.out.println("Email Number"+(i+1));
-            System.out.println("Subject: "+message.getSubject());
-            System.out.println("From: "+message.getFrom()[0]);
-
+        Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
             }
+        });
 
-            //close the store and folder objects
-            emailFolder.close(true);
-            store.close();
+        try {
+            Message message = new MimeMessage(session);
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmailAddress));
+            message.setSubject(subject);
+            message.setText(textMessage);
 
-        } catch (NoSuchProviderException e) {
+
+            System.out.println("\nyour message received successfully....");
+
         } catch (MessagingException e) {
-        }catch (Exception e){
+            throw new RuntimeException(e);
         }
     }
-
-    public static void main(String[] args) {
-
-        String host = "pop.gmail.com";// change accordingly
-        String mailStoreType = "pop3";
-        String username = "ksravya1411@gmail.com";// change accordingly
-        String password = "*****";// change accordingly
-
-        check(host, mailStoreType, username, password);
-
-    }
-
 }
